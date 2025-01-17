a294ae80d429996ad9955ecbd851c709c1b7fcce
tcharlottin <tcharlottin@gmail.com>
2025-01-17 11:12:48 +0100

    add the stability results

--- src/detect_string_instability_platoon.py
+++ src/detect_string_instability_platoon.py
@@ -1,12 +1,8 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
 import matplotlib.pyplot as plt
 import ast
 import os
-
-sns.set_theme()
-sns.set(font_scale=1.2)
 
 def retieve_3_ACC_platoon(platoon_df, trajs_df):
     ACC_ids = pd.unique(trajs_df[trajs_df['ACC'] == 'Yes']['ID'])
@@ -29,6 +25,13 @@ def create_string_instability_test_vectors(platoon_compo, t_min, t_max, trajs_df
     
     return speed_vector
 
+def truncate_speed_vectors(speed_vectors):
+    valid_lengths = np.apply_along_axis(lambda col: np.max(np.where(~np.isnan(col))) + 1 if not np.all(np.isnan(col)) else 0, axis=1, arr=speed_vectors)
+    max_valid_length = np.max(valid_lengths)
+    speed_vectors_truncated = speed_vectors[:, :max_valid_length]
+    
+    return speed_vectors_truncated
+
 def calculate_p_norms(speed_vectors):
     diff_vectors = np.diff(speed_vectors, axis=0)
     norm_2 = np.linalg.norm(diff_vectors, axis=1)  # 2-norm
@@ -36,17 +39,21 @@ def calculate_p_norms(speed_vectors):
     return norm_2, norm_inf
 
 def string_stability_test(speed_vectors):
-    norm_2, norm_inf = calculate_p_norms(speed_vectors)
-    strict_stability = (norm_inf[:-1] / norm_inf[1:] < 1).all()
-    weak_stability = (norm_2[:-1] / norm_2[1:] < 1).all()
-    
-    return 'Stable' if strict_stability else 'Unstable', 'Stable' if weak_stability else 'Unstable'
+    speed_vectors_truncated = truncate_speed_vectors(speed_vectors)
+    norm_2, norm_inf = calculate_p_norms(speed_vectors_truncated)
+    strict_stability  = 'Stable'
+    weak_stability = 'Stable'
+    for k in range(1,len(norm_2)):
+        if  norm_inf[k] / norm_inf[k-1] < 1:
+            strict_stability = 'Unstable'
+        if norm_inf[k] / norm_inf[0] < 1:
+            weak_stability = 'Unstable'
+    return weak_stability, strict_stability
 
 def add_stability_information(trajs_df, platoon_df):
     platoon_df['platoon compo'] = platoon_df['platoon compo'].apply(ast.literal_eval)
     strict_stability = []
     weak_stability = []
-    
     for k in range(len(platoon_df['platoon compo'])):
         platoon_compo = [platoon_df['leader'][k]] + platoon_df['platoon compo'][k]
         tmin = platoon_df['time'][k]
@@ -55,7 +62,6 @@ def add_stability_information(trajs_df, platoon_df):
         strict, weak = string_stability_test(speed_vectors)
         strict_stability.append(strict)
         weak_stability.append(weak)
-        
     platoon_df['strict stability'] = strict_stability
     platoon_df['weak stability'] = weak_stability
     return platoon_df
@@ -76,10 +82,13 @@ def routine(path_platoon,path_trajs):
     files = os.listdir(path_platoon)
     for f in files :
         platoon_df = pd.read_csv(path_platoon+f)
+        platoon_df = platoon_df[['time','leader','platoon compo','string compo',
+                                'platoon DTW','ACC',
+                                'platoon with 3 ACC','platoon with 2 ACC','platoon with 1 ACC']]
         trajs_df = pd.read_csv(path_trajs+f)
         platoon_df = add_stability_information(trajs_df,platoon_df)
         platoon_df = retieve_3_ACC_platoon(platoon_df,trajs_df)
-        platoon_df.to_csv(path_platoon+f)
+        platoon_df.to_csv(path_platoon+f,index=False)
     
 path_platoon = 'out/platoons/'
 path_trajs = 'data/by_run/corrected/'
